ae60c6aef9121725abf3f7a602e446160e83a246
Hayoung Kang <rkdgkdudsam@naver.com>
2026-02-27 14:49:00 +0900

    feat: 새로고침 개선 (스크롤, 화면 깜빡임)

--- src/routes/layouts/RootLayout.tsx
+++ src/routes/layouts/RootLayout.tsx
@@ -1,8 +1,14 @@
 import Cursor from "@/components/atoms/Cursor";
 import CursorBackground from "@/components/atoms/CursorBackground";
+import { useEffect } from "react";
 import { Outlet } from "react-router";
 
 export default function RootLayout() {
+    useEffect(() => {
+        const id = setTimeout(() => window.scrollTo({ top: 0 }), 0);
+        return () => clearTimeout(id);
+    }, []);
+
     return (
         <>
             <Cursor />
